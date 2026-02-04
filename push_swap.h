a0b6e5a6b326dc7117165a6aa14a96f5f3db7ce9
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-02-04 17:07:39 +0300

    Make main file + finishing operations

--- push_swap.h
+++ push_swap.h
@@ -1,26 +1,31 @@
 #ifndef push_swap_h
 #define push_swap_h
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-typedef struct t_node
-{
-    int content;
-    struct t_node *next;
+
+typedef struct t_node {
+  int content;
+  struct t_node *next;
 } t_node;
 
-typedef struct t_stack
-{
-    int size;
-    t_node *head;
+typedef struct t_stack {
+  int size;
+  t_node *head;
 } t_stack;
 
+void rra(t_stack *stack);
+void rrb(t_stack *stack);
+void rrr(t_stack *stack_a, t_stack *stack_b);
 void swap_top(t_stack *stack);
 void sa(t_stack *a);
 void sb(t_stack *b);
 void ss(t_stack *a, t_stack *b);
-void push_top(t_stack *stack_throw,t_stack *stack_host);
+void push_top(t_stack *stack_throw, t_stack *stack_host);
 void pa(t_stack *a, t_stack *b);
 void pb(t_stack *a, t_stack *b);
-void rotate(t_stack *stack);
+void rotate_up(t_stack *stack);
+void rotate_down(t_stack *stack);
 void ra(t_stack *a);
 void rb(t_stack *b);
 void rr(t_stack *a, t_stack *b);
