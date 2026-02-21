be2c50fc1424f33662ea615900e25f42a840ff2f
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-02-21 07:22:37 +0300

    Fixing norm

--- sorting.h
+++ sorting.h
@@ -6,17 +6,19 @@
 /*   By: aqoraan <aqoraan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 02:34:14 by aqoraan           #+#    #+#             */
-/*   Updated: 2026/02/21 06:18:37 by aqoraan          ###   ########.fr       */
+/*   Updated: 2026/02/21 07:22:12 by aqoraan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SORTING_H
-#define SORTING_H
-typedef struct pair {
-  int first;
-  int second;
-} t_pair;
-int *rank(int *arr, int size);
-void radix_sort(int *arr, int size);
+# define SORTING_H
+
+typedef struct pair
+{
+	int	first;
+	int	second;
+}		t_pair;
+int		*rank(int *arr, int size);
+void	radix_sort(int *arr, int size);
 
 #endif
--- src/parsing-normalization/4_normalization.c
+++ src/parsing-normalization/4_normalization.c
@@ -17,79 +17,89 @@
  *
  */
 
-static int check_sign(char *string, int *idx, int *count_digit) {
-  int count_neg;
-  int count_pos;
+static int	check_sign(char *string, int *idx, int *count_digit)
+{
+	int	count_neg;
+	int	count_pos;
 
-  count_neg = 0;
-  count_pos = 0;
-  while (string[(*idx)] == '-' || string[(*idx)] == '+') {
-    count_digit += 1;
-    if (string[(*idx)] == '-')
-      count_neg += 1;
-    if (string[(*idx)] == '+')
-      count_pos += 1;
-    (*idx) += 1;
-  }
-  if (count_neg > 1 || count_pos > 1 || (count_neg == 1 && count_pos == 1))
-    return (0);
-  return (1);
+	count_neg = 0;
+	count_pos = 0;
+	while (string[(*idx)] == '-' || string[(*idx)] == '+')
+	{
+		count_digit += 1;
+		if (string[(*idx)] == '-')
+			count_neg += 1;
+		if (string[(*idx)] == '+')
+			count_pos += 1;
+		(*idx) += 1;
+	}
+	if (count_neg > 1 || count_pos > 1 || (count_neg == 1 && count_pos == 1))
+		return (0);
+	return (1);
 }
 
-static int check_string(char *string) {
-  int idx;
-  int count_digit;
-  long long number;
-  int count_neg;
-  int count_pos;
+static int	check_string(char *string)
+{
+	int			idx;
+	int			count_digit;
+	long long	number;
+	int			count_neg;
+	int			count_pos;
 
-  idx = 0;
-  count_digit = 0;
-  if (!check_sign(string, &idx, &count_digit))
-    return (0);
-  while (string[idx]) {
-    count_digit += 1;
-    if (!(string[idx] >= '0' && string[idx] <= '9') || count_digit > 11)
-      return (0);
-    idx += 1;
-  }
-  number = ft_atoll(string);
-  if (number < -2147483648 || number > 2147483647)
-    return (0);
-  return (1);
+	idx = 0;
+	count_digit = 0;
+	if (!check_sign(string, &idx, &count_digit))
+		return (0);
+	while (string[idx])
+	{
+		count_digit += 1;
+		if (!(string[idx] >= '0' && string[idx] <= '9') || count_digit > 11)
+			return (0);
+		idx += 1;
+	}
+	number = ft_atoll(string);
+	if (number < -2147483648 || number > 2147483647)
+		return (0);
+	return (1);
 }
 
-static int count_valid_int(char **split, int *count) {
-  int j;
+static int	count_valid_int(char **split, int *count)
+{
+	int	j;
 
-  j = 0;
-  while (split[j]) {
-    if (check_flag(split[j])) {
-      j += 1;
-      continue;
-    } else if (check_string(split[j]))
-      (*count) += 1;
-    else
-      return (0);
-    j += 1;
-  }
-  return (1);
+	j = 0;
+	while (split[j])
+	{
+		if (check_flag(split[j]))
+		{
+			j += 1;
+			continue ;
+		}
+		else if (check_string(split[j]))
+			(*count) += 1;
+		else
+			return (0);
+		j += 1;
+	}
+	return (1);
 }
 
-int normalization(int argc, char **string) {
-  int i;
-  char **split;
-  int j;
-  int count;
+int	normalization(int argc, char **string)
+{
+	int		i;
+	char	**split;
+	int		j;
+	int		count;
 
-  i = 1;
-  count = 0;
-  while (i < argc) {
-    split = ft_split(string[i], ' ');
-    if (!count_valid_int(split, &count))
-      return (0);
-    free_split(split);
-    i += 1;
-  }
-  return (count);
+	i = 1;
+	count = 0;
+	while (i < argc)
+	{
+		split = ft_split(string[i], ' ');
+		if (!count_valid_int(split, &count))
+			return (0);
+		free_split(split);
+		i += 1;
+	}
+	return (count);
 }
