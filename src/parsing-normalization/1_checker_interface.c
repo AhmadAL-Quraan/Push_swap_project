b64eda6da6791fff70b5c0c71e35cb4b3c874eaf
Ahmad AL-Quraan <ahmadquraan.git@gmail.com>
2026-02-21 03:08:56 +0300

    Adding ranking algo + sorting structure

--- src/parsing-normalization/1_checker_interface.c
+++ src/parsing-normalization/1_checker_interface.c
@@ -6,7 +6,7 @@
 /*   By: aqoraan <aqoraan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 20:07:49 by aqoraan           #+#    #+#             */
-/*   Updated: 2026/02/20 06:10:52 by aqoraan          ###   ########.fr       */
+/*   Updated: 2026/02/21 02:18:28 by aqoraan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,20 @@
  *
  * */
 
-static void	error(t_flags *flags)
-{
-	free(flags);
-	write(1, "Error\n", 6);
-	exit(1);
+static void error(t_flags *flags) {
+  free(flags);
+  write(1, "Error\n", 6);
+  exit(1);
 }
 
-void	start_checker(int argc, char **string)
-{
-	t_flags	*flags;
-	int		*arr;
-	int		parse_size;
-	int		i;
+void start_checker(int argc, char **string, int **arr, t_flags **flags) {
 
-	flags = malloc(sizeof(t_flags));
-	flags->adaptive = 2;
-	parse_size = parsing(argc, string, &arr);
-	if (!check_flags(string, flags) || !parse_size)
-		error(flags);
+  int parse_size;
+  int i;
+
+  (*flags) = malloc(sizeof(t_flags));
+  (*flags)->adaptive = 2;
+  parse_size = parsing(argc, string, arr);
+  if (!check_flags(string, flags) || !parse_size)
+    error(*flags);
 }
